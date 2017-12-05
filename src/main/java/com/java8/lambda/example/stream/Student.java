a5aa6ebf26c40a030e5d1583ee1576cdef28a69a
魏喆 <Richard@vpn184.its.manchester.ac.uk>
2017-12-05 10:20:48 +0800

    flatMap

---
+++ src/main/java/com/java8/lambda/example/stream/Student.java
@@ -0,0 +1,31 @@
+package com.java8.lambda.example.stream;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Richard on 17/11/3.
+ */
+public class Student {
+
+    private String name;
+    private Set<String> book;
+
+    public void addBook(String book) {
+        if (this.book == null) {
+            this.book = new HashSet<>();
+        }
+        this.book.add(book);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<String> getBook() {
+        return book;
+    }
+    // getters and setters
+
+
+}
---
+++ src/main/java/com/java8/lambda/example/stream/flatmap/FlatMapToSet.java
@@ -0,0 +1,59 @@
+package com.java8.lambda.example.stream.flatmap;
+
+import com.java8.lambda.example.stream.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by Richard on 17/12/5.
+ */
+public class FlatMapToSet {
+
+    public static void main(String[] args) {
+
+        System.out.println("flatMap() and Set example:");
+
+        Student obj1 = new Student();
+        obj1.setName("Richard");
+        obj1.addBook("Java 8 in Action");
+        obj1.addBook("Spring Boot in Action");
+        obj1.addBook("Effective Java (2nd Edition)");
+
+        Student obj2 = new Student();
+        obj2.setName("Zilap");
+        obj2.addBook("Learning Python, 5th Edition");
+        obj2.addBook("Effective Java (2nd Edition)");
+
+        List<Student> list = new ArrayList<>();
+        list.add(obj1);
+        list.add(obj2);
+
+        List<String> collect = list.stream()
+                                .map(x -> x.getBook())      // Stream<Set<String>>
+                                .flatMap(x -> x.stream())   // Stream<String>
+                                .distinct()
+                                .collect(Collectors.toList());
+
+        collect.forEach(x -> System.out.println(x));
+
+        System.out.println();
+        System.out.println("Stream + Primitive + flatMapToInt:");
+
+        int[] intArray = {1, 2, 3, 4, 5, 6};
+
+        // 1. Stream<int[]>
+        Stream<int[]> streamArray = Stream.of(intArray);
+
+        // 2. Stream<int[]> -> flatMap -> IntStream
+        IntStream intStream = streamArray.flatMapToInt(x -> Arrays.stream(x));
+
+        intStream.forEach(x -> System.out.println(x));
+
+
+    }
+}
